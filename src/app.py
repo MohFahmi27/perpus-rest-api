fb1f7a7f1a747ecf401b49967f8bb2b02ee446c0
MohFahmi27 <mohammadfahmi417@gmail.com>
2022-05-07 21:18:20 +0800

    clean code + implement detail data peminjaman

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from enum import unique
 from lib2to3.pgen2 import token
 from operator import and_
 from os import link
+from unittest import result
 from webbrowser import get
 from xmlrpc.client import boolean
 
@@ -39,7 +40,8 @@ policy = PasswordPolicy.from_names(
     numbers=1,  # need min. 1 digits
 )
 
-# ===== DATABASE MODELS ===== 
+# ===== DATABASE MODELS =====
+
 
 class BukuModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -52,21 +54,28 @@ class BukuModel(db.Model):
     def __repr__(self) -> str:
         return f"Buku(isbn = {self.isbn}, judul_buku = {self.judul_buku}, pengarang = {self.pengarang}, tahun_terbit = {self.tahun_terbit}, qty_buku = {self.qty_buku}"
 
+
 buku_put_args = reqparse.RequestParser()
-buku_put_args.add_argument("isbn", type=str, help="ISBN is required", required=True)
-buku_put_args.add_argument("judul_buku", type=str, help="Judul Buku is required", required=True)
-buku_put_args.add_argument("pengarang", type=str, help="Pengarang is required", required=True)
-buku_put_args.add_argument("tahun_terbit", type=str, help="Tahun Terbit is required", required=True)
-buku_put_args.add_argument("qty_buku", type=int, help="Quantity Buku is required", required=True)
+buku_put_args.add_argument(
+    "isbn", type=str, help="ISBN is required", required=True)
+buku_put_args.add_argument("judul_buku", type=str,
+                           help="Judul Buku is required", required=True)
+buku_put_args.add_argument("pengarang", type=str,
+                           help="Pengarang is required", required=True)
+buku_put_args.add_argument("tahun_terbit", type=str,
+                           help="Tahun Terbit is required", required=True)
+buku_put_args.add_argument(
+    "qty_buku", type=int, help="Quantity Buku is required", required=True)
 
 resource_fields_buku = {
-    'id' : fields.Integer,
-	'isbn': fields.String,
-	'judul_buku': fields.String,
-	'pengarang': fields.String,
-	'tahun_terbit': fields.String,
-	'qty_buku': fields.Integer
+    'id': fields.Integer,
+    'isbn': fields.String,
+    'judul_buku': fields.String,
+    'pengarang': fields.String,
+    'tahun_terbit': fields.String,
+    'qty_buku': fields.Integer
 }
+
 
 class SiswaModel(db.Model):
     nim = db.Column(db.String(9), nullable=False,  primary_key=True)
@@ -76,31 +85,40 @@ class SiswaModel(db.Model):
     def __repr__(self) -> str:
         return f"Buku(nim_mahasiswa = {self.nim}, nama = {self.nama}, no_hp = {self.no_hp}"
 
+
 mahasiswa_put_args = reqparse.RequestParser()
-mahasiswa_put_args.add_argument("nim", type=str, help="NIM is required", required=True)
-mahasiswa_put_args.add_argument("nama", type=str, help="Nama is required", required=True)
-mahasiswa_put_args.add_argument("no_hp", type=str, help="No Hp is required", required=True)
+mahasiswa_put_args.add_argument(
+    "nim", type=str, help="NIM is required", required=True)
+mahasiswa_put_args.add_argument(
+    "nama", type=str, help="Nama is required", required=True)
+mahasiswa_put_args.add_argument(
+    "no_hp", type=str, help="No Hp is required", required=True)
 
 resource_fields_mahasiswa = {
-    'nim' : fields.String,
-	'nama': fields.String,
-	'no_hp': fields.String
+    'nim': fields.String,
+    'nama': fields.String,
+    'no_hp': fields.String
 }
+
 
 class PetugasModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nama = db.Column(db.String(45), nullable=False)
     no_hp = db.Column(db.String(13), nullable=False)
 
+
 petugas_put_args = reqparse.RequestParser()
-petugas_put_args.add_argument("nama", type=str, help="Nama is required", required=True)
-petugas_put_args.add_argument("no_hp", type=str, help="No Telp is required", required=True)
+petugas_put_args.add_argument(
+    "nama", type=str, help="Nama is required", required=True)
+petugas_put_args.add_argument(
+    "no_hp", type=str, help="No Telp is required", required=True)
 
 resource_fields_petugas = {
-    'id' : fields.Integer,
-    'nama' : fields.String,
-	'no_hp': fields.String
-} 
+    'id': fields.Integer,
+    'nama': fields.String,
+    'no_hp': fields.String
+}
+
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -109,20 +127,24 @@ class UserModel(db.Model):
     token = db.Column(db.String(50), nullable=False)
     verify_status = db.Column(db.Boolean, default=False)
 
+
 user_put_args = reqparse.RequestParser()
-user_put_args.add_argument("email", type=str, help="Email is required", required=True)
-user_put_args.add_argument("password", type=str, help="Password is required", required=True)
+user_put_args.add_argument(
+    "email", type=str, help="Email is required", required=True)
+user_put_args.add_argument(
+    "password", type=str, help="Password is required", required=True)
 
 resource_fields_user = {
-    'email' : fields.String,
-	'password': fields.String,
-	'token': fields.String,
-	'verify_status': fields.Boolean
-}    
+    'email': fields.String,
+    'password': fields.String,
+    'token': fields.String,
+    'verify_status': fields.Boolean
+}
 
 resource_fields_user_sign_in = {
-	'token': fields.String
-}    
+    'token': fields.String
+}
+
 
 class PeminjamanModel(db.Model):
     id_peminjaman = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -132,44 +154,68 @@ class PeminjamanModel(db.Model):
     nim_mahasiswa = db.Column(db.String(9), ForeignKey(SiswaModel.nim))
     id_petugas = db.Column(db.Integer, ForeignKey(PetugasModel.id))
 
+    def json(self):
+        return {'id_peminjaman': self.id_peminjaman, 'tanggal_peminjaman': self.tanggal_peminjaman, 'tanggal_kembali': self.tanggal_kembali, 'denda': self.denda, 'nim_mahasiswa': self.nim_mahasiswa, 'id_petugas': self.id_petugas}
+
+
 peminjaman_put_args = reqparse.RequestParser()
-peminjaman_put_args.add_argument("nim_mahasiswa", type=str, help="NIM is required", required=True)
-peminjaman_put_args.add_argument("id_petugas", type=str, help="ID Petugas is required", required=True)
+peminjaman_put_args.add_argument(
+    "nim_mahasiswa", type=str, help="NIM is required", required=True)
+peminjaman_put_args.add_argument(
+    "id_petugas", type=str, help="ID Petugas is required", required=True)
 
 resource_fields_peminjaman = {
-    'id_peminjaman' : fields.Integer,
-	'tanggal_peminjaman': fields.String,
-	'tanggal_kembali': fields.String,
-	'denda': fields.Integer,
-	'nim_mahasiswa': fields.String,
-	'id_petugas': fields.Integer
-}    
-
-class PeminjamanDetailModel(db.Model):
-    id_detail_peminjaman = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_peminjaman = db.Column(db.Integer, ForeignKey(PeminjamanModel.id_peminjaman))
-    id_buku = db.Column(db.Integer, ForeignKey(BukuModel.id))
-
-peminjaman_detail_put_args = reqparse.RequestParser()
-peminjaman_detail_put_args.add_argument("id_buku", type=int, help="ID BUKU is required", required=True)
-
-resource_fields_peminjaman_detail = {
-    'id_detail_peminjaman' : fields.Integer,
-	'id_peminjaman': fields.Integer,
-	'id_buku': fields.Integer
-} 
-
-resource_fields_get_peminjaman = {
-    'id_peminjaman' : fields.Integer,
-	'tanggal_peminjaman': fields.String,
-	'tanggal_kembali': fields.String,
-	'denda': fields.Integer,
-	'nim_mahasiswa': fields.String,
-	'id_petugas': fields.Integer
+    'id_peminjaman': fields.Integer,
+    'tanggal_peminjaman': fields.String,
+    'tanggal_kembali': fields.String,
+    'denda': fields.Integer,
+    'nim_mahasiswa': fields.String,
+    'id_petugas': fields.Integer
 }
 
 
-# ===== ENDPOINTS CONFIGURE ===== 
+class PeminjamanDetailModel(db.Model):
+    id_detail_peminjaman = db.Column(
+        db.Integer, primary_key=True, autoincrement=True)
+    id_peminjaman = db.Column(
+        db.Integer, ForeignKey(PeminjamanModel.id_peminjaman))
+    id_buku = db.Column(db.Integer, ForeignKey(BukuModel.id))
+
+    def __str__(self):
+        return f'id_detail: {self.id_detail_peminjaman}, id_peminjaman: {self.id_peminjaman}, id_buku: {self.id_buku}'
+
+
+peminjaman_detail_put_args = reqparse.RequestParser()
+peminjaman_detail_put_args.add_argument(
+    "id_buku", type=int, help="ID BUKU is required", required=True)
+
+resource_fields_peminjaman_detail = {
+    'id_detail_peminjaman': fields.Integer,
+    'id_peminjaman': fields.Integer,
+    'id_buku': fields.Integer
+}
+
+resource_fields_peminjaman_data_list = {
+    'id_peminjaman': fields.Integer,
+    'tanggal_peminjaman': fields.String,
+    'tanggal_kembali': fields.String,
+    'denda': fields.Integer,
+    'nim_mahasiswa': fields.String,
+    'id_petugas': fields.Integer,
+    'detail_data': fields.Nested(resource_fields_peminjaman_detail)
+}
+
+resource_fields_get_peminjaman = {
+    'id_peminjaman': fields.Integer,
+    'tanggal_peminjaman': fields.String,
+    'tanggal_kembali': fields.String,
+    'denda': fields.Integer,
+    'nim_mahasiswa': fields.String,
+    'id_petugas': fields.Integer
+}
+
+
+# ===== ENDPOINTS CONFIGURE =====
 
 class get_all_buku(Resource):
     @marshal_with(resource_fields_buku)
@@ -180,8 +226,9 @@ class get_all_buku(Resource):
 
         if(user == 1):
             return BukuModel.query.all(), 200
-        else :
+        else:
             abort(403, message="Forbidden: Authentication token doesn't exist")
+
 
 class search_buku(Resource):
     @marshal_with(resource_fields_buku)
@@ -193,8 +240,9 @@ class search_buku(Resource):
 
         if(user == 1):
             return BukuModel.query.filter(BukuModel.judul_buku.like(f"%{query}%")).all(), 200
-        else :
+        else:
             abort(403, message="Forbidden: Authentication token doesn't exist")
+
 
 class detail_buku(Resource):
     @marshal_with(resource_fields_buku)
@@ -210,22 +258,26 @@ class detail_buku(Resource):
         else:
             abort(403, message="Forbidden: Authentication token doesn't exist")
 
+
 class add_buku(Resource):
     @marshal_with(resource_fields_buku)
     def post(self):
         args = buku_put_args.parse_args()
-        buku = BukuModel(isbn=args['isbn'], judul_buku=args['judul_buku'], pengarang=args['pengarang'], tahun_terbit=args['tahun_terbit'], qty_buku=args['qty_buku'])
+        buku = BukuModel(isbn=args['isbn'], judul_buku=args['judul_buku'], pengarang=args['pengarang'],
+                         tahun_terbit=args['tahun_terbit'], qty_buku=args['qty_buku'])
         try:
             db.session.add(buku)
             db.session.commit()
             return buku, 200
         except Exception:
-            abort(400, message=f"Bad Request: Something went wrong!")  
+            abort(400, message=f"Bad Request: Something went wrong!")
+
 
 class get_all_mahasiswa(Resource):
     @marshal_with(resource_fields_mahasiswa)
     def get(self):
         return SiswaModel.query.all()
+
 
 class detail_mahasiswa(Resource):
     @marshal_with(resource_fields_mahasiswa)
@@ -235,17 +287,20 @@ class detail_mahasiswa(Resource):
             abort(404, message="Could not find anything")
         return result
 
+
 class add_mahasiswa(Resource):
     @marshal_with(resource_fields_mahasiswa)
     def post(self):
         args = mahasiswa_put_args.parse_args()
-        mahasiswa = SiswaModel(nim=args['nim'], nama=args['nama'], no_hp=args['no_hp'])
+        mahasiswa = SiswaModel(
+            nim=args['nim'], nama=args['nama'], no_hp=args['no_hp'])
         try:
             db.session.add(mahasiswa)
             db.session.commit()
             return mahasiswa, 200
         except Exception:
-            abort(400, message=f"Bad Request: NIM already been used")  
+            abort(400, message=f"Bad Request: NIM already been used")
+
 
 class add_petugas(Resource):
     @marshal_with(resource_fields_petugas)
@@ -257,7 +312,8 @@ class add_petugas(Resource):
             db.session.commit()
             return petugas, 200
         except Exception as e:
-            abort(400, message=f"Bad Request: Something went wrong {e}")  
+            abort(400, message=f"Bad Request: Something went wrong {e}")
+
 
 class detail_petugas(Resource):
     @marshal_with(resource_fields_petugas)
@@ -267,19 +323,24 @@ class detail_petugas(Resource):
             abort(404, message="Could not find anything")
         return result, 200
 
+
 class add_peminjaman(Resource):
     @marshal_with(resource_fields_peminjaman)
     def post(self):
         args = peminjaman_put_args.parse_args()
         try:
-            mahasiswa = SiswaModel.query.filter_by(nim=args['nim_mahasiswa']).first()
-            petugas = PetugasModel.query.filter_by(id=args['id_petugas']).first()
-            peminjaman = PeminjamanModel(tanggal_peminjaman=date.today(), tanggal_kembali=(date.today()) + timedelta(weeks=2), nim_mahasiswa=mahasiswa.nim, id_petugas=petugas.id)
+            mahasiswa = SiswaModel.query.filter_by(
+                nim=args['nim_mahasiswa']).first()
+            petugas = PetugasModel.query.filter_by(
+                id=args['id_petugas']).first()
+            peminjaman = PeminjamanModel(tanggal_peminjaman=date.today(), tanggal_kembali=(
+                date.today()) + timedelta(weeks=2), nim_mahasiswa=mahasiswa.nim, id_petugas=petugas.id)
             db.session.add(peminjaman)
             db.session.commit()
             return peminjaman, 201
         except Exception as e:
-            abort(400, message=f"Bad Request: Something went wrong {e}")  
+            abort(400, message=f"Bad Request: Something went wrong {e}")
+
 
 class add_detail_peminjaman(Resource):
     @marshal_with(resource_fields_peminjaman_detail)
@@ -289,61 +350,90 @@ class add_detail_peminjaman(Resource):
         if (buku.qty_buku <= 0):
             abort(400, message=f"Bad Request: Buku Stock Not Available")
         try:
-            peminjaman = PeminjamanModel.query.filter_by(id_peminjaman=id_peminjaman).first()  
-            peminjaman_detail = PeminjamanDetailModel(id_peminjaman=peminjaman.id_peminjaman, id_buku=buku.id)
+            peminjaman = PeminjamanModel.query.filter_by(
+                id_peminjaman=id_peminjaman).first()
+            peminjaman_detail = PeminjamanDetailModel(
+                id_peminjaman=peminjaman.id_peminjaman, id_buku=buku.id)
             buku.qty_buku = buku.qty_buku - 1
             db.session.add(peminjaman_detail)
             db.session.commit()
             return peminjaman_detail, 201
         except Exception as e:
-            abort(400, message=f"Bad Request: Something went wrong | {e}")  
+            abort(400, message=f"Bad Request: Something went wrong | {e}")
+
 
 class get_detail_peminjaman(Resource):
     @marshal_with(resource_fields_peminjaman_detail)
     def get(self, id_peminjaman):
-        result = PeminjamanDetailModel.query.filter_by(id_peminjaman=id_peminjaman).all()
+        result = PeminjamanDetailModel.query.filter_by(
+            id_peminjaman=id_peminjaman).all()
         if not result:
             abort(404, message="Could not find anything")
         return result
+
+
+class get_complete_data_peminjaman(Resource):
+    @marshal_with(resource_fields_peminjaman_data_list)
+    def get(self, id_peminjaman):
+        peminjaman_result = PeminjamanModel.query.filter_by(
+            id_peminjaman=id_peminjaman).first()
+        if not peminjaman_result:
+            abort(404, message="Could not find anything")
+
+        detail_result = PeminjamanDetailModel.query.filter_by(
+            id_peminjaman=id_peminjaman).all()
+        result = {'id_peminjaman': peminjaman_result.id_peminjaman, 'tanggal_peminjaman': peminjaman_result.tanggal_peminjaman, 'tanggal_kembali': peminjaman_result.tanggal_kembali,
+                  'denda': peminjaman_result.denda, 'nim_mahasiswa': peminjaman_result.nim_mahasiswa, 'id_petugas': peminjaman_result.id_petugas, 'detail_data': detail_result}
+        return result
+
 
 class sign_up(Resource):
     @marshal_with(resource_fields_user)
     def post(self):
         args = user_put_args.parse_args()
-        checkUserAvability = UserModel.query.filter_by(email=args['email']).count()
+        checkUserAvability = UserModel.query.filter_by(
+            email=args['email']).count()
         if (checkUserAvability >= 1):
             abort(400, message="Bad Request: Email already used")
         if policy.test(args['password']):
             abort(400, message="Bad Request: Password not strong enough | password atleast 8 characters, Has A digit, and Has Uppercase Letter")
 
         try:
-            generated_token = secretEncodeDummy.dumps(args['email'], salt='email_verification')
-            user = UserModel(email=args['email'], password=args['password'], token=generated_token)
+            generated_token = secretEncodeDummy.dumps(
+                args['email'], salt='email_verification')
+            user = UserModel(
+                email=args['email'], password=args['password'], token=generated_token)
             db.session.add(user)
             db.session.commit()
 
             # Send Email
             email = args['email']
-            msg = Message('Verifikasi Email Perpustakaan', sender='mohfahmi270@gmail.com', recipients=[email])
-            link = url_for('confirm_email', token=generated_token, _external=True)
+            msg = Message('Verifikasi Email Perpustakaan',
+                          sender='mohfahmi270@gmail.com', recipients=[email])
+            link = url_for('confirm_email',
+                           token=generated_token, _external=True)
             msg.body = f'Silahkan klik link berikut ini untuk memverifikasi akun anda: {link}'
             mail.send(msg)
             return user, 201
         except Exception as e:
-            abort(400, message=f"Bad Request: Something went wrong {e}")     
+            abort(400, message=f"Bad Request: Something went wrong {e}")
+
 
 class sign_in(Resource):
     @marshal_with(resource_fields_user_sign_in)
     def post(self):
         args = user_put_args.parse_args()
-        login = UserModel.query.filter(and_(UserModel.email==args['email'], UserModel.password==args['password'])).first()
+        login = UserModel.query.filter(and_(
+            UserModel.email == args['email'], UserModel.password == args['password'])).first()
         if not login:
-            abort(400, message="Bad Request: Login Unsuccessfull Check email and password")
+            abort(
+                400, message="Bad Request: Login Unsuccessfull Check email and password")
 
         if(login.verify_status == True):
-            return login, 200   
-        else :
+            return login, 200
+        else:
             abort(401, message="Unauthorized: User Not Verify")
+
 
 @app.route('/confirm_email/<token>', methods=['GET'])
 def confirm_email(token):
@@ -355,10 +445,11 @@ def confirm_email(token):
     except SignatureExpired:
         return jsonify({"message": "ERROR: Token Has Been Expired"}), 401
     except Exception as e:
-        abort(400, message=f"Bad Request: Something went Wrong: {e}")  
+        abort(400, message=f"Bad Request: Something went Wrong: {e}")
     return jsonify({"message": "OK: Email Have been successfully verify"}), 201
 
-# ===== ENDPOINTS ===== 
+# ===== ENDPOINTS =====
+
 
 # AUTH
 api.add_resource(sign_up, "/signup")
@@ -382,8 +473,10 @@ api.add_resource(add_petugas, "/petugas")
 # PEMINJAMAN
 api.add_resource(add_peminjaman, "/peminjaman")
 api.add_resource(add_detail_peminjaman, "/peminjaman/<int:id_peminjaman>")
-api.add_resource(get_detail_peminjaman, "/peminjaman/<int:id_peminjaman>/detail")
-
+api.add_resource(get_detail_peminjaman,
+                 "/peminjaman/<int:id_peminjaman>/detail")
+api.add_resource(get_complete_data_peminjaman,
+                 "/peminjaman/data/<int:id_peminjaman>")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
